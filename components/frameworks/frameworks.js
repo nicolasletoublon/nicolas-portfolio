da0a12fc0cdad9b7787e35223b0ef0eac37b7cd4
nicolas.letoublon <nicolas.letoublon@kronos.com>
2016-03-15 13:45:52 -0400

    Changes logos

--- components/frameworks/frameworks.js
+++ components/frameworks/frameworks.js
@@ -17,9 +17,9 @@ function frameworksController() {
 
     self.littleLogos = [
         {href: "https://angularjs.org/", image: "angular", col: 2, row: 2},
-        {href: "http://stackoverflow.com/questions/796087/make-a-div-into-a-link", image: "react", col: 2, row: 2},
+        {href: "https://facebook.github.io/react/", image: "react", col: 2, row: 2},
         {href: "http://gruntjs.com/", image: "grunt", col: 2, row: 2},
-        {href: "https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3", image: "bootstrap", col: 2, row: 2},
+        {href: "http://getbootstrap.com/", image: "bootstrap", col: 2, row: 2},
         {href: "https://www.mongodb.org/", image: "mongo", col: 2, row: 2},
         {href: "http://www.material-ui.com/#/", image: "mui", col: 2, row: 2}
     ];
@@ -27,9 +27,9 @@ function frameworksController() {
     self.bigLogos = [
         {href: "http://ionicframework.com/", image: "ionic", col: 4, row: 2},
         {href: "https://angularjs.org/", image: "angular", col: 2, row: 2},
-        {href: "http://stackoverflow.com/questions/796087/make-a-div-into-a-link", image: "react", col: 2, row: 2},
+        {href: "https://facebook.github.io/react/", image: "react", col: 2, row: 2},
         {href: "http://gruntjs.com/", image: "grunt", col: 2, row: 2},
-        {href: "https://developer.mozilla.org/en-US/docs/Web/CSS/CSS3", image: "bootstrap", col: 2, row: 2},
+        {href: "http://getbootstrap.com/", image: "bootstrap", col: 2, row: 2},
         {href: "http://www.material-ui.com/#/", image: "mui", col: 2, row: 2},
         {href: "https://nodejs.org/en/", image: "node", col: 4, row: 2},
         {href: "https://www.mongodb.org/", image: "mongo", col: 2, row: 2}
